821661ac90cc3b0a3da283957f536fd54eea41ee
Brian <mukwazhi@gmail.com>
2024-06-26 13:59:29 +0200

    urls re -refactor

--- tipoff/tipoff/urls.py
+++ tipoff/tipoff/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(" ", include("report.urls")),
+    path("report", include("report.urls")),
 ]
