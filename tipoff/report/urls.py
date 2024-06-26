0fa315339fbd2d251f8da63920227bc42abf1615
Brian <mukwazhi@gmail.com>
2024-06-26 14:12:27 +0200

    urls -refactor

--- tipoff/report/urls.py
+++ tipoff/report/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.report_form, name="report_form"),
+    path('', views.report_form, name="report_form"),
     path("thanks", views.thanks),
     ]
